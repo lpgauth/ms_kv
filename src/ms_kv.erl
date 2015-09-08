7003d8d5f64385be80e43a98fa3746ea8b45f530
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-08 16:45:10 +0530

    Add basic read repair

--- src/ms_kv.erl
+++ src/ms_kv.erl
@@ -1,6 +1,8 @@
 -module(ms_kv).
 -include("ms_kv.hrl").
 
+-compile({no_auto_import, [put/2]}).
+
 -export([
     get/1,
     put/2
@@ -17,7 +19,7 @@ get(Key) ->
             DbRef = ms_kv_db:ref(),
             case ms_kv_db:get(Key, DbRef) of
                 not_found ->
-                    not_found;
+                    read_repair(Key);
                 {ok, Value} ->
                     ms_kv_cache:put(Key, Value),
                     {ok, Value}
@@ -29,3 +31,13 @@ get(Key) ->
 put(Key, Value) ->
     DbRef = ms_kv_db:ref(),
     ms_kv_db:put(Key, Value, DbRef).
+
+%% private
+read_repair(Key) ->
+    case ms_base:apply_all_not_local(?MODULE, ms_kv, get, [Key]) of
+        [] ->
+            not_found;
+        [Value | _] ->
+            put(Key, Value),
+            Value
+    end.
