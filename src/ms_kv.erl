900bfdea1a9715bf47cdaaf563df27d115213996
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-09 10:01:18 +0530

    Add metrics

--- src/ms_kv.erl
+++ src/ms_kv.erl
@@ -8,18 +8,24 @@
     put/2
 ]).
 
+-define(SAMPLE_RATE, 1).
+
 %% public
 -spec get(binary()) -> {ok, binary()} | not_found.
 
 get(Key) ->
+    ms_base_metric:increment(<<"ms_kv.get.call">>, 1, ?SAMPLE_RATE),
     case ms_base:apply_hash(?APP, Key, ms_kv_cache, get, [Key]) of
         {ok, Value} ->
+            ms_base_metric:increment(<<"ms_kv.get.cache">>, 1, ?SAMPLE_RATE),
             {ok, Value};
         _ ->
             case ms_kv_db:get(Key) of
                 not_found ->
+                    ms_base_metric:increment(<<"ms_kv.get.not_found">>, 1, ?SAMPLE_RATE),
                     read_repair(Key);
                 {ok, Value} ->
+                    ms_base_metric:increment(<<"ms_kv.get.db">>, 1, ?SAMPLE_RATE),
                     ms_base:apply_hash(?APP, Key, ms_kv_cache, put,
                         [Key, Value]),
                     {ok, Value}
